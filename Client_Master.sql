e056b5359988ca9f998896848d98cbec9e4a32f7
Aayush <119883228+Aayushh2108@users.noreply.github.com>
2023-06-19 23:04:38 +0530

    Update Client_Master.sql

--- Client_Master.sql
+++ Client_Master.sql
@@ -1,2 +1,3 @@
 CREATE TABLE Client_Master(Client_No VARCHAR2(6),Name VARCHAR2(20),Address1  VARCHAR2(30),Address2 VARCHAR2(30),City VARCHAR2(15),
-State VARCHAR2(15),Pincode number(6),Bal_Due number(10,2))
+State VARCHAR2(15),Pincode number(6),Bal_Due number(10,2));
+
